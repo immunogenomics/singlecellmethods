96c904ac75e226f959f4422d54897e5f98112b5c
Ilya Korsunsky <ilya.korsunsky@gmail.com>
2018-11-26 16:07:24 -0500

    updated types

--- src/.ipynb_checkpoints/methods-checkpoint.cpp
+++ src/.ipynb_checkpoints/methods-checkpoint.cpp
@@ -7,15 +7,15 @@ using namespace arma;
 
 //typedef arma::mat MATTYPE;
 //typedef arma::vec VECTYPE;
-typedef arma::fmat MATTYPE;
-typedef arma::fvec VECTYPE;
+//typedef arma::fmat MATTYPE;
+//typedef arma::fvec VECTYPE;
 
 
 
 // [[Rcpp::export]]
-MATTYPE exp_mean(const VECTYPE& x, const VECTYPE& p, const VECTYPE& i, int ncol, int nrow, const uvec& groups, const uvec& group_sizes) {
+arma::mat exp_mean(const arma::vec& x, const VECTYPE& p, const arma::vec& i, int ncol, int nrow, const uvec& groups, const uvec& group_sizes) {
     int ngroups = group_sizes.n_elem;
-    MATTYPE res = zeros<MATTYPE>(nrow, ngroups);
+    arma::mat res = arma::zeros<arma::mat>(nrow, ngroups);
     for (int c = 0; c < ncol; c++) {
         for (int j = p[c]; j < p[c + 1]; j++) {
             // i[j] gives the row num
@@ -35,13 +35,13 @@ MATTYPE exp_mean(const VECTYPE& x, const VECTYPE& p, const VECTYPE& i, int ncol,
 
 
 // [[Rcpp::export]]
-MATTYPE log_vmr(const VECTYPE& x, const VECTYPE& p, const VECTYPE& i, 
-                  int ncol, int nrow, const MATTYPE& means,
+arma::mat log_vmr(const arma::vec& x, const arma::vec& p, const arma::vec& i, 
+                  int ncol, int nrow, const arma::mat& means,
                   const uvec& groups, const uvec& group_sizes) {
     
     int ngroups = group_sizes.n_elem;
-    MATTYPE res = zeros<MATTYPE>(nrow, ngroups);
-    MATTYPE nnzero = zeros<MATTYPE>(nrow, ngroups);
+    arma::mat res = arma::zeros<arma::mat>(nrow, ngroups);
+    arma::mat nnzero = arma::zeros<arma::mat>(nrow, ngroups);
     double tmp;
     for (int c = 0; c < ncol; c++) {
         for (int j = p[c]; j < p[c + 1]; j++) {

e8ebcf114cbc196c97375f5d89b5c5354bb738c8
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-09-16 10:17:30 -0400

    modified vargenes to keep gene names the same

--- R/findVariableGenes.R
+++ R/findVariableGenes.R
@@ -7,21 +7,19 @@ findVariableGenes <- function(X, groups, min_expr = .1, max_expr = Inf,
     groups_table <- table(groups_int)
     
     ## initially compute means in non-log space, to use in vmr function below
-    means_nonlog <- exp_mean(X@x, X@p, X@i, ncol(X), nrow(X), groups_int, groups_table)
+    means_nonlog <- singlecellmethods:::exp_mean(X@x, X@p, X@i, ncol(X), nrow(X), groups_int, groups_table)
     colnames(means_nonlog) <- levels(groups)
-    row.names(means_nonlog) <- row.names(X)
     
-    vmr <- log_vmr(X@x, X@p, X@i, ncol(X), nrow(X), means_nonlog, groups_int, groups_table)    
-    colnames(vmr) <- levels(groups)
-    row.names(vmr) <- row.names(X)
-    
+    vmr <- singlecellmethods:::log_vmr(X@x, X@p, X@i, ncol(X), nrow(X), means_nonlog, groups_int, groups_table)    
+    colnames(vmr) <- levels(groups)    
+
     ## transform means to logspace and join means and VMR  
     vargenes_df <- dplyr::inner_join(
-        means_nonlog %>% log1p %>% as.data.frame() %>% 
-            tibble::rownames_to_column("symbol") %>% 
+        means_nonlog %>% log1p %>% tibble() %>% 
+            cbind(symbol = row.names(X)) %>% 
             tidyr::gather(group, gene_mean, -symbol),
-        vmr %>% as.data.frame() %>% 
-            tibble::rownames_to_column("symbol") %>% 
+        vmr %>% tibble() %>% 
+            cbind(symbol = row.names(X)) %>% 
             tidyr::gather(group, gene_dispersion, -symbol), 
         by = c("symbol", "group")
     )

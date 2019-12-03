28c26898a20ad2ff9c0e8d360f9a783b055c165b
Ilya Korsunsky <ilya.korsunsky@gmail.com>
2019-12-03 17:05:26 -0500

    add pipe

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @examples
+#' x <- 5 %>% sum(10)
+#' 
+#' @usage lhs \%>\% rhs
+#' @return return value of rhs function. 
+NULL

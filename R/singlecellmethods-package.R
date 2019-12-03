b7051fbc8817563dad3734344c3839854cd54d52
Ilya Korsunsky <ilya.korsunsky@gmail.com>
2019-12-03 17:02:08 -0500

    export

---
+++ R/singlecellmethods-package.R
@@ -0,0 +1,11 @@
+#' @name singlecellmethods
+#' @docType package
+#' @useDynLib singlecellmethods
+#' @importFrom Rcpp sourceCpp
+#' @importFrom Rcpp loadModule
+#' @importFrom methods new
+#' @importFrom methods as
+#' @importFrom methods is
+#' @importFrom rlang .data
+#' @importFrom data.table data.table
+NULL

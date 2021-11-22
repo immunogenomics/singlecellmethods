c4104a5f0bcfde150e350d0791bd4f275c63eacd
Ilya Korsunsky <ilya.korsunsky@gmail.com>
2021-11-22 14:55:23 -0500

    added Seurat wrapper for balanced pca

--- R/PCA.R
+++ R/PCA.R
@@ -22,18 +22,14 @@ weighted_pca <- function(X, weights, genes_use=NULL, npc=20, do_corr=TRUE, scale
     }
     
     ## weighted z-scores
-#     mu <- X %>% apply(1, function(x) {SDMTools:::wt.mean(x, weights)})
-#     sig <- X %>% apply(1, function(x) {SDMTools:::wt.sd(x, weights)})
     mu <- rowMeans(X, weights = weights)
     sig <- rowSDs(X, weights = weights)
     
-    # Added 12/9/19: save weighted scaling means and std devs
     vargenes_means_sds <- tibble(
         symbol = genes_use,
         mean = mu
     )
     vargenes_means_sds$stddev <- sig
-    # finish added 12/9/19
     
     X <- scaleDataWithStats(X, mu, sig) 
     X <- X[which(is.na(rowSums(X)) == 0), ]
@@ -42,7 +38,6 @@ weighted_pca <- function(X, weights, genes_use=NULL, npc=20, do_corr=TRUE, scale
     }
     
     ## weighted SVD
-#     pres <- rsvd::rsvd(X %*% Matrix::Diagonal(x = sqrt(weights)), k = npc)
     pres <- RSpectra::svds(X %*% Matrix::Diagonal(x = sqrt(weights)), npc)
     V <- (Matrix::Diagonal(x = 1 / sqrt(weights)) %*% pres$v) %*% diag(pres$d)
     V <- as.matrix(V)
@@ -52,3 +47,39 @@ weighted_pca <- function(X, weights, genes_use=NULL, npc=20, do_corr=TRUE, scale
     row.names(pres$u) <- row.names(X)
     return(list(loadings = pres$u, embeddings = V, vargenes = vargenes_means_sds))
 }
+
+
+## Wrapper to Seurat objects
+RunBalancedPCA <- function(obj, weight.by='orig.ident', npcs=20, assay.use='RNA', reduction.name = "pca", reduction.key = "PC_") {
+    if (!weight.by %in% colnames(obj@meta.data)) 
+        stop(glue('Variable {weight.by} not defined in this object'))
+    if (length(unique(obj@meta.data[[weight.by]])) == 1) 
+        stop(glue('Only 1 level found for {weight.by}. Balanced PCA requires at least 2.'))
+    if (any(is.na(obj@meta.data[[weight.by]])))
+        stop(glue('Balanced PCA must have non-NA values in {weight.by} variable for all cells'))
+    
+    y <- obj@meta.data[[weight.by]]
+    weights <- as.numeric(((1 / table(y))[y]) * (length(y) / length(unique(y))))    
+    pca_res <- weighted_pca(
+        obj@assays[[assay.use]]@data, 
+        weights, 
+        obj@assays[[assay.use]]@var.features,
+        npcs,
+        scale_thresh=10
+    )
+
+    ## Put it back into Seurat 
+    obj@assays[[assay.use]]@meta.features <- obj@assays[[assay.use]]@meta.features %>% 
+        tibble::rownames_to_column('symbol') %>% 
+        left_join(dplyr::rename(pca_res$vargenes, balanced_pca_mean = mean, balanced_pca_stddev = stddev), by = 'symbol') %>% 
+        tibble::column_to_rownames('symbol')
+    obj[[reduction.name]] <- Seurat::CreateDimReducObject(
+        embeddings = pca_res$embeddings,
+        loadings = pca_res$loadings, 
+        stdev = as.numeric(apply(pca_res$embeddings, 2, stats::sd)),
+        assay = assay.use,
+        key = reduction.key
+    )
+    return(obj)
+}
+
